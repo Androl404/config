2a70e9353f849bf54ecb48de005373d182b0b026
Andrei Zeucianu <andrei.zeucianu@gmail.com>
2024-12-29 02:19:05 +0100

    Updated Org-mode configuration for Emacs
    Added hook for flyspell-mode, disabled emphasis hiding, removed fancy
    font configuration because it's better when it's simpler.
    I also put a lot of code into a function which is called when a frame
    is created, because a window error is generated when lauching the
    Emacs daemon.
    Removed validation button at the end of Org-mode's HTML exports.

--- emacs/org.el
+++ emacs/org.el
@@ -7,44 +7,48 @@
 ;; To use special bullets for lists
 (add-hook 'org-mode-hook 'org-bullets-mode)
 (add-hook 'org-mode-hook 'visual-line-mode)
+(add-hook 'org-mode-hook 'flyspell-mode)
 ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
 
 ;; Pour masquer les caractères de mise en forme, comme les `*', `/' ou `_'
-(setq org-hide-emphasis-markers t)
+;; (setq org-hide-emphasis-markers t)
 
 ;; To use little bullets for lists
 (font-lock-add-keywords 'org-mode
                         '(("^ *\\([-]\\) "
                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-;;  Pour paramétrer des polices
-(let* ((variable-tuple
-        (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-              ((x-list-fonts "Verdana")         '(:font "Verdana"))
-              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-       (headline           `(:inherit default :weight bold)))
+(defun set-org-fonts ()
+  ;;  Pour paramétrer des polices
+  ;; (message-box "On passe par cette fonction !") ;; Pour le débuggage
+  (let* ((variable-tuple
+          (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+                ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+         (headline           `(:inherit default :weight bold)))
+    (custom-theme-set-faces
+     'user
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+  ;; For the line number font, to be Iosevka everywhere
+  ;; (set-face-attribute 'line-number nil :inherit 'fixed)
+
+  ;; To have custom font
   (custom-theme-set-faces
    'user
-   `(org-level-8 ((t (,@headline ,@variable-tuple))))
-   `(org-level-7 ((t (,@headline ,@variable-tuple))))
-   `(org-level-6 ((t (,@headline ,@variable-tuple))))
-   `(org-level-5 ((t (,@headline ,@variable-tuple))))
-   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-   `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
-
-;; For the line number font, to be Iosevka everywhere
-;; (set-face-attribute 'line-number nil :inherit 'fixed)
-
-;; To have custom font
-(custom-theme-set-faces
- 'user
- '(variable-pitch ((t (:family "Roboto" :height 180 :weight regular))))
- '(fixed-pitch ((t ( :family "Iosevka Nerd Font" :height 160)))))
+   '(variable-pitch ((t (:family "Roboto" :height 180 :weight regular))))
+   '(fixed-pitch ((t ( :family "Iosevka Nerd Font" :height 160))))))
+(add-hook 'server-after-make-frame-hook 'set-org-fonts)
 
 ;; To enable different backend export options
 (require 'ox-md)
@@ -53,6 +57,9 @@
 ;; To allow syntax highlighting in HTML
 (require 'ox-html)
 (setq org-html-htmlize-output-type 'css)
+
+;; To remove validate button at the end of HTML exports
+(setq org-html-validation-link nil)
 
 ;; Function to include the CSS style into the HTML when exporting from Org-Mode
 (defun my-org-inline-css-hook (exporter)
