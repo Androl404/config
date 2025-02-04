9dd39bdb5c5a943e0fb8fdb2ac00bae47302a7ec
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-02-04 17:40:50 +0100

    Added new configuration for Rofi

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 # For gVim (Vim with graphical interface)
 ln -sf "$SCRIPT_DIR/vim/.gvimrc" ~/.gvimrc
 # For tmux
-ln -sf "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf 
+ln -sf "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf
 
 # Verify if .config directory exists
 mkdir -p ~/.config
@@ -17,16 +17,18 @@ mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/alacritty/
 mkdir -p ~/.config/kitty/
 mkdir -p ~/.config/i3/
+mkdir -p ~/.config/rofi/
+mkdir -p ~/.config/rofi/themes/
 mkdir -p ~/.emacs.d/
 
 # For NeoVim
 ln -sf "$SCRIPT_DIR/neovim/init.lua" ~/.config/nvim/init.lua
 
 # For Alacritty
-ln -sf "$SCRIPT_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml 
+ln -sf "$SCRIPT_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
 
 # For Alacritty
-ln -sf "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf 
+ln -sf "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 
 # For i3wm
 ln -sf "$SCRIPT_DIR/i3wm/config" ~/.config/i3/config
@@ -38,3 +40,9 @@ ln -sf "$SCRIPT_DIR/emacs/init.el" ~/.emacs.d/init.el
 ln -sf "$SCRIPT_DIR/emacs/custom.el" ~/.emacs.d/custom.el
 ln -sf "$SCRIPT_DIR/emacs/org.el" ~/.emacs.d/org.el
 ln -sf "$SCRIPT_DIR/emacs/org-style.css" ~/.emacs.d/org-style.css
+
+# For Rofi
+ln -sf "$SCRIPT_DIR/rofi/config.rasi" ~/.config/rofi/config.rasi
+ln -sf "$SCRIPT_DIR/rofi/themes/dtos-center-new.rasi" ~/.config/rofi/themes/dtos-center-new.rasi
+ln -sf "$SCRIPT_DIR/rofi/themes/dtos-center.rasi" ~/.config/rofi/themes/dtos-center.rasi
+ln -sf "$SCRIPT_DIR/rofi/themes/dtos-dmenu.rasi" ~/.config/rofi/themes/dtos-dmenu.rasi
