faf9ec20c822bdc3700fd16bcbfc68538461dfaf
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-02-10 20:33:26 +0100

    [emacs] Added files to load in the copilot file

--- emacs/enable_copilot.el
+++ emacs/enable_copilot.el
@@ -2,6 +2,10 @@
 ;; Then evaluate copilot_balanced.el and copilot.el in Emacs to load the package.
 ;; Then evaluate the following lines.
 
+;; Load the filecopilot_balanced.el
+(load-file "/media/andrei/Data/dev/third_repo/copilot.el/copilot-balancer.el")
+(load-file "/media/andrei/Data/dev/third_repo/copilot.el/copilot")
+
 (require 'copilot)
 (add-hook 'prog-mode-hook 'copilot-mode)
 (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
