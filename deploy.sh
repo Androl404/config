346f6cf2410bc068e950a88de06efe627bf389f0
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-03-04 19:07:33 +0100

    Removed unecessary stuff from deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,12 @@ ln -sf "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim/
 mkdir -p ~/.config/alacritty/
-mkdir -p ~/.config/kitty/
+# mkdir -p ~/.config/kitty/
 mkdir -p ~/.config/i3/
 mkdir -p ~/.config/rofi/
 mkdir -p ~/.config/rofi/themes/
 mkdir -p ~/.emacs.d/
-mkdir -p ~/.screenlayout/
+# mkdir -p ~/.screenlayout/
 
 # For NeoVim
 ln -sf "$SCRIPT_DIR/neovim/init.lua" ~/.config/nvim/init.lua
@@ -29,7 +29,7 @@ ln -sf "$SCRIPT_DIR/neovim/init.lua" ~/.config/nvim/init.lua
 ln -sf "$SCRIPT_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
 
 # For Kitty
-ln -sf "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+# ln -sf "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 
 # For i3wm
 ln -sf "$SCRIPT_DIR/i3wm/config" ~/.config/i3/config
@@ -49,9 +49,9 @@ ln -sf "$SCRIPT_DIR/rofi/themes/dtos-center.rasi" ~/.config/rofi/themes/dtos-cen
 ln -sf "$SCRIPT_DIR/rofi/themes/dtos-dmenu.rasi" ~/.config/rofi/themes/dtos-dmenu.rasi
 
 # For the screenlayout script
-ln -sf "$SCRIPT_DIR/screenlayout/home.sh" ~/.screenlayout/home.sh 
-ln -sf "$SCRIPT_DIR/screenlayout/home_second_screen.sh" ~/.screenlayout/home_second_screen.sh 
-ln -sf "$SCRIPT_DIR/screenlayout/laptop_screen.sh" ~/.screenlayout/laptop_screen.sh 
-ln -sf "$SCRIPT_DIR/screenlayout/duplicate_1080.sh" ~/.screenlayout/duplicate_1080.sh 
-ln -sf "$SCRIPT_DIR/screenlayout/extend_1080.sh" ~/.screenlayout/extend_1080.sh 
-ln -sf "$SCRIPT_DIR/screenlayout/extend_1440.sh" ~/.screenlayout/extend_1440.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/home.sh" ~/.screenlayout/home.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/home_second_screen.sh" ~/.screenlayout/home_second_screen.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/laptop_screen.sh" ~/.screenlayout/laptop_screen.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/duplicate_1080.sh" ~/.screenlayout/duplicate_1080.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/extend_1080.sh" ~/.screenlayout/extend_1080.sh 
+# ln -sf "$SCRIPT_DIR/screenlayout/extend_1440.sh" ~/.screenlayout/extend_1440.sh 
