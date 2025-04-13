b50769f36bfd3a94a3d338b0064d45d824b2a9da
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-04-13 17:08:08 +0200

    [emacs] Added binding to smex for menu button

--- emacs/init.el
+++ emacs/init.el
@@ -49,6 +49,7 @@
 ;; To set up smex, in order to have a custom M-x menu, which the most use commands first
 (require 'smex)
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "<menu>") 'smex)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; To set indetation as 4 spaces for C (Is it really needed?)
