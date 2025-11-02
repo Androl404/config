049de33af75992656bf6f1e26e4120248d27e888
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-11-02 13:46:30 +0100

    Update deploy script and README

--- README.md
+++ README.md
@@ -12,4 +12,26 @@ Here are the programs which the config is stored in this repo:
 * Hyprland
 * Waybar
 
-The font [Iosevka Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip) and [Iosevka](https://github.com/be5invis/Iosevka/releases) are required in order for the config to work properly.
+The font [Iosevka Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip) are required in order for the config to work properly. Multiple others font are necessary for org mode.
+
+## Usage
+
+Install everything:
+``` bash
+./deploy.sh install
+```
+
+Install only some tools:
+``` bash
+./deploy.sh install emacs rofi nvim
+```
+
+Remove (uninstall) configs for a specific tool:
+``` bash
+./deploy.sh uninstall rofi
+```
+
+Uninstall everything:
+``` bash
+./deploy.sh uninstall all
+```
--- deploy.sh
+++ deploy.sh
@@ -1,63 +1,86 @@
-#!/bin/sh
-
-# TODO: Make a function which creates the simlink
-# Because the actual system is not easily portable
-
+#!/usr/bin/env sh
 set -e
-SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 
-# For gVim (Vim with graphical interface)
-ln -sf "$SCRIPT_DIR/vim/.gvimrc" ~/.gvimrc
-# For tmux
-ln -sf "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+ACTION="$1"       # install | uninstall
+shift             # Remaining arguments are program names (optional)
 
-# Verify if .config directory exists
-mkdir -p ~/.config
-mkdir -p ~/.config/nvim/
-mkdir -p ~/.config/alacritty/
-mkdir -p ~/.config/kitty/
-mkdir -p ~/.config/i3/
-mkdir -p ~/.config/rofi/
-mkdir -p ~/.config/rofi/themes/
-mkdir -p ~/.config/hyprland/
-mkdir -p ~/.config/waybar/
-mkdir -p ~/.config/sway/
-mkdir -p ~/.emacs.d/
+# --- Helper functions --------------------------------------------------------
 
-# For NeoVim
-ln -sf "$SCRIPT_DIR/neovim/init.lua" ~/.config/nvim/init.lua
+install_link() {
+    src="$SCRIPT_DIR/$1"
+    dest="$2"
+    mkdir -p "$(dirname "$dest")"
+    ln -sf "$src" "$dest"
+    printf "➕ Linked %s → %s\n" "$src" "$dest"
+}
 
-# For Alacritty
-ln -sf "$SCRIPT_DIR/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
+remove_link() {
+    dest="$2"
+    if [ -L "$dest" ]; then
+        rm "$dest"
+        printf "Removed symlink %s\n" "$dest"
+    fi
+}
 
-# For Kitty
-ln -sf "$SCRIPT_DIR/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+# --- Config table ------------------------------------------------------------
+# PROGRAM | source relative to repo | target absolute path
 
-# For i3wm
-ln -sf "$SCRIPT_DIR/i3wm/config" ~/.config/i3/config
-ln -sf "$SCRIPT_DIR/i3wm/.i3status.conf" ~/ # .config/i3/config
+CONFIG="
+vim      vim/.gvimrc                      ~/.gvimrc
+tmux     tmux/.tmux.conf                  ~/.tmux.conf
+nvim     neovim/init.lua                  ~/.config/nvim/init.lua
+alacritty alacritty/alacritty.toml        ~/.config/alacritty/alacritty.toml
+kitty    kitty/kitty.conf                 ~/.config/kitty/kitty.conf
+i3       i3wm/config                      ~/.config/i3/config
+i3       i3wm/.i3status.conf              ~/.i3status.conf
+hypr     hyprland/hyprland.conf           ~/.config/hyprland/hyprland.conf
+sway     sway/config                       ~/.config/sway/config
+waybar   waybar/config                     ~/.config/waybar/config
+waybar   waybar/style.css                  ~/.config/waybar/style.css
+rofi     rofi/config.rasi                  ~/.config/rofi/config.rasi
+rofi     rofi/leave.sh                     ~/.config/rofi/leave.sh
+rofi     rofi/themes/dtos-center-new.rasi  ~/.config/rofi/themes/dtos-center-new.rasi
+rofi     rofi/themes/dtos-center.rasi      ~/.config/rofi/themes/dtos-center.rasi
+rofi     rofi/themes/dtos-dmenu.rasi       ~/.config/rofi/themes/dtos-dmenu.rasi
+emacs    emacs/init.el                     ~/.emacs.d/init.el
+emacs    emacs/rc.el                       ~/.emacs.d/rc.el
+emacs    emacs/custom.el                   ~/.emacs.d/custom.el
+emacs    emacs/org.el                      ~/.emacs.d/org.el
+emacs    emacs/magit-windows.el            ~/.emacs.d/magit-windows.el
+emacs    emacs/w32-browser.el              ~/.emacs.d/w32-browser.el
+emacs    emacs/org-style.css               ~/.emacs.d/org-style.css
+"
 
-# For Emacs
-ln -sf "$SCRIPT_DIR/emacs/rc.el" ~/.emacs.d/rc.el
-ln -sf "$SCRIPT_DIR/emacs/init.el" ~/.emacs.d/init.el
-ln -sf "$SCRIPT_DIR/emacs/custom.el" ~/.emacs.d/custom.el
-ln -sf "$SCRIPT_DIR/emacs/org.el" ~/.emacs.d/org.el
-ln -sf "$SCRIPT_DIR/emacs/git-windows.el" ~/.emacs.d/git-windows.el
-ln -sf "$SCRIPT_DIR/emacs/org-style.css" ~/.emacs.d/org-style.css
+# --- Program selection --------------------------------------------------------
+if [ $# -eq 0 ]; then
+    PROGRAMS="all"
+else
+    PROGRAMS="$@"
+fi
 
-# For Rofi
-ln -sf "$SCRIPT_DIR/rofi/config.rasi" ~/.config/rofi/config.rasi
-ln -sf "$SCRIPT_DIR/rofi/themes/dtos-center-new.rasi" ~/.config/rofi/themes/dtos-center-new.rasi
-ln -sf "$SCRIPT_DIR/rofi/themes/dtos-center.rasi" ~/.config/rofi/themes/dtos-center.rasi
-ln -sf "$SCRIPT_DIR/rofi/themes/dtos-dmenu.rasi" ~/.config/rofi/themes/dtos-dmenu.rasi
-ln -sf "$SCRIPT_DIR/rofi/leave.sh" ~/.config/rofi/leave.sh
+install_or_remove() {
+    program="$1"
+    action="$2"
 
-# For Hyprland
-ln -sf "$SCRIPT_DIR/hyprland/hyprland.conf" ~/.config/hyprland/hyprland.conf
+    printf "\n== %s %s ==\n" "$action" "$program"
 
-# For Waybar
-ln -sf "$SCRIPT_DIR/waybar/config" ~/.config/waybar/config
-ln -sf "$SCRIPT_DIR/waybar/style.css" ~/.config/waybar/style.css
+    echo "$CONFIG" | while read -r prog src dest; do
+        [ -z "$prog" ] && continue
 
-# For sway
-ln -sf "$SCRIPT_DIR/sway/config" ~/.config/sway/config
+        if [ "$program" = "all" ] || [ "$program" = "$prog" ]; then
+            case $action in
+                install) install_link "$src" "$dest" ;;
+                uninstall) remove_link "$src" "$dest" ;;
+            esac
+        fi
+    done
+}
+
+# --- Dispatch ----------------------------------------------------------------
+
+for p in $PROGRAMS; do
+    install_or_remove "$p" "$ACTION"
+done
+
+printf "\nDone.\n"
