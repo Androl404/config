f3878cd640019f3b5321ae73ac87121b9436d090
Andrei Zeucianu <benjaminpotron@gmail.com>
2026-06-14 17:24:40 +0200

    [fish] Add exeution time of process in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
         set -l last_status $status
+        set -l last_duration $CMD_DURATION
         set -l normal (set_color normal)
         set -l status_color (set_color brgreen)
         set -l cwd_color (set_color $fish_color_cwd)
@@ -24,7 +25,14 @@ function fish_prompt --description 'Write out the prompt'
                 set status_color (set_color $fish_color_error)
                 set prompt_status $status_color "[" $last_status "]" $normal
         end
-    
-        echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+
+        # Format duration if it exists and is non-zero
+        set -l duration_str ""
+        if test -n "$last_duration" -a "$last_duration" -gt 0
+            set -l seconds (math "$last_duration / 1000")
+            set duration_str (set_color yellow) "took " (printf '%.2fs' $seconds) " " $normal
+        end
+
+        echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status ' ' $duration_str
         echo -n -s $status_color $suffix ' ' $normal
 end
